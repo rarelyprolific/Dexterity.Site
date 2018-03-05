451ad436eb38db0cf4349b06f1a7feb179e52297
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2018-03-05 18:28:28 +0000

    Added empty Site, Auth and Login projects before beginning to implement authentication

--- Dexterity.Login/Startup.cs
+++ Dexterity.Login/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace Dexterity.Site
+namespace Dexterity.Login
 {
     public class Startup
     {
