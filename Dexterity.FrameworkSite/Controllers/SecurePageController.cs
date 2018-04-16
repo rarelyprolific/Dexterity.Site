24464181807dd374adc0fcf33ecd134a10c6bfd7
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2018-04-16 21:56:45 +0100

    Added Dexterity.FrameworkSite project to test securing a .NET Framework 4.6.1 web application against Identity Server 4

---
+++ Dexterity.FrameworkSite/Controllers/SecurePageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dexterity.FrameworkSite.Controllers
+{
+    [Authorize]
+    public class SecurePageController : Controller
+    {
+        // GET: SecurePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
