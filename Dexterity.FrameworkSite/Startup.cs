e576bc6230f95d229ad1e027d7173a82ce18f723
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2018-05-14 21:46:02 +0100

    Work in progress attempt at getting custom claims coming through to a Katana OWIN client

--- Dexterity.FrameworkSite/Startup.cs
+++ Dexterity.FrameworkSite/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 using Microsoft.Owin;
 using Microsoft.Owin.Security;
 using Microsoft.Owin.Security.Cookies;
@@ -21,7 +23,7 @@ namespace Dexterity.FrameworkSite
 
         public void Configuration(IAppBuilder app)
         {
-            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            //JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
 
             app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
             app.UseCookieAuthentication(new CookieAuthenticationOptions());
@@ -40,12 +42,66 @@ namespace Dexterity.FrameworkSite
 
                 //// TODO: No claims coming through yet!? Figure out why! Also, how can I see the id_token to see if they are there?
 
-                TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                //TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                //{
+                //    NameClaimType = "name",
+                //    RoleClaimType = "role",
+                //},
+
+                //// TODO: Look at example at https://github.com/IdentityServer/IdentityServer3.Samples/tree/master/source/MVC%20Authentication/EmbeddedMvc
+                Notifications = new OpenIdConnectAuthenticationNotifications
                 {
-                    NameClaimType = "name",
-                    RoleClaimType = "role",
+                    SecurityTokenValidated = async n =>
+                    {
+                        var nid = new ClaimsIdentity(
+                            n.AuthenticationTicket.Identity.AuthenticationType,
+                            "given_name",
+                            "role");
+                        //Constants.ClaimTypes.GivenName,
+                        //Constants.ClaimTypes.Role);
+
+                        // get userinfo data
+                        //var userInfoClient = new UserInfoClient(
+                        //    new Uri(n.Options.Authority + "/connect/userinfo"),
+                        //    n.ProtocolMessage.AccessToken);
+
+                        //var userInfo = await userInfoClient.GetAsync();
+                        //userInfo.Claims.ToList().ForEach(ui => nid.AddClaim(new Claim(ui.Item1, ui.Item2)));
+
+                        // keep the id_token for logout
+                        nid.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+                        // add access token for sample API
+                        //nid.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+
+                        // keep track of access token expiration
+                        //nid.AddClaim(new Claim("expires_at", DateTimeOffset.Now.AddSeconds(int.Parse(n.ProtocolMessage.ExpiresIn)).ToString()));
+
+                        // add some other app specific claim
+                        nid.AddClaim(new Claim("app_specific", "some data"));
+
+                        n.AuthenticationTicket = new AuthenticationTicket(
+                            nid,
+                            n.AuthenticationTicket.Properties);
+                    },
+
+                    //RedirectToIdentityProvider = n =>
+                    //{
+                    //    if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                    //    {
+                    //        var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+                    //        if (idTokenHint != null)
+                    //        {
+                    //            n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+                    //        }
+                    //    }
+
+                    //    return Task.FromResult(0);
+                    //}
                 }
             });
+
 
             // ************************************************************
             // **** This is what worked in the .NET Core 2 application ****
